67b049217fefba5006515dc9a628abd1c391c1ac
TennY-prog <teniolaoyebade200@gmail.com>
2026-01-12 12:55:07 +0000

    updated typescript

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,7 @@
 "use strict";
+// let message: string = "Aryan";
+// console.log(message)
 Object.defineProperty(exports, "__esModule", { value: true });
-let message = "Aryan";
-console.log(message);
 // Number (includes integers, floats, hex, binary)
 let age = 25;
 let price = 19.99;
@@ -26,3 +26,44 @@ let uncertain = "Hello";
 if (typeof uncertain === "string") {
     console.log(uncertain.toUpperCase()); //Type narrowed
 }
+// Arrays - Collections of same type
+// let numbers: number[] = [1, 2, 3, 4];
+// * Readonly arrays (immutable)
+let numbers = [1, 2, 3, 4, 5];
+// numbers.push(90) // ERROR - cannot modify
+console.log(numbers);
+// * `Tuples` : a Tuple is a special type of array that has a fixed number of elements where each element has a specific type.
+// let user: [string, number, boolean] = ["Ibraheem", 29, true];
+// console.log(user);
+// * Named tuples (Typescript 4.0+)
+// let point: [Teniola: number, Oyebade: number] = [10, 20];
+// console.log(point[0]);
+// const [xCoordinate, yCoordinate] = point;
+// console.log(xCoordinate);
+let point = [10, 20, "Opebe", 90];
+const [, , print, another_print] = point;
+console.log(print);
+// * Optional Tuple elements
+let optionalTuple = ["hello"];
+let tuple = ["hello", 42, true, false, true];
+let response = [true, "Data fetched", { id: 1 }]; // `{id: 1}` is an object datatype.
+let message = "Hello";
+const user1 = {
+    id: 101,
+    name: "Alice"
+};
+const user2 = {
+    id: "EMP-99",
+    name: "Bod",
+    email: "bob@example.com"
+};
+console.log(user2);
+// * Implementation
+const user = {
+    id: 9098,
+    name: "Teniola",
+    greet() {
+        return `Hello, my name is ${this.name}`;
+    }
+};
+console.log(user.greet());
--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,8 @@
 
 
 
-let message: string = "Aryan";
-console.log(message)
+// let message: string = "Aryan";
+// console.log(message)
 
 
 // Number (includes integers, floats, hex, binary)
@@ -36,3 +36,91 @@ let uncertain: unknown = "Hello";
 if(typeof uncertain === "string"){
     console.log(uncertain.toUpperCase()); //Type narrowed
 }
+
+// Arrays - Collections of same type
+// let numbers: number[] = [1, 2, 3, 4];
+
+// * Readonly arrays (immutable)
+let numbers: readonly number[] = [1, 2, 3, 4, 5];
+// numbers.push(90) // ERROR - cannot modify
+console.log(numbers);
+
+// * `Tuples` : a Tuple is a special type of array that has a fixed number of elements where each element has a specific type.
+
+// let user: [string, number, boolean] = ["Ibraheem", 29, true];
+// console.log(user);
+
+// * Named tuples (Typescript 4.0+)
+// let point: [Teniola: number, Oyebade: number] = [10, 20];
+// console.log(point[0]);
+// const [xCoordinate, yCoordinate] = point;
+// console.log(xCoordinate);
+
+let point: [a: number, b: number, c: string, d: number] = [10, 20, "Opebe", 90];
+const [, , print, another_print] = point;
+console.log(print);
+
+
+// * Optional Tuple elements
+let optionalTuple: [string, number?] = ["hello"];
+
+// * Rest elements in tuples
+type justAName = [string, number, ...boolean[]]; // "..." is the `Rest` operator
+let tuple: justAName = ["hello", 42, true, false, true];
+
+// Practical tuple example: API response
+type ApiResponse = [boolean, string, any?]; // [success, message, data?]
+let response : ApiResponse = [true, "Data fetched", {id: 1}]; // `{id: 1}` is an object datatype.
+
+
+// Type Aliases vs Interfaces
+
+// A Type Alias in Typescript in essentially a custom name (or nickname) for a specific type
+
+type MyString = string;
+let message: MyString = "Hello";
+
+// * TYPE ALIAS - For creating custom types
+type UserID = string | number;
+type Status = "pending" | "approved" | "rejected";
+
+// Combining types
+type BasicUser = {
+    id: UserID;
+    name: string;
+    email?: string; //Optional property
+};
+
+const user1: BasicUser = {
+    id: 101,
+    name: "Alice"
+};
+
+const user2: BasicUser = {
+    id: "EMP-99",
+    name: "Bod",
+    email: "bob@example.com"
+}
+console.log(user2);
+
+//*** `Interface` : An Interface in Typescript is a rulebook (or a contract) that defines the structure an object.
+
+// It tells Typescript: "I expect an object to look exactly like this: it must have these properties, and they must be specific types."
+
+interface IUser{
+    id: UserID;
+    name: string;
+    email?: "string";
+    greet(): string;
+}
+
+// * Implementation
+const user: IUser = {
+    id: 9098,
+    name: "Teniola",
+    greet() {
+        return `Hello, my name is ${this.name}`;
+    }
+};
+
+console.log(user.greet());
